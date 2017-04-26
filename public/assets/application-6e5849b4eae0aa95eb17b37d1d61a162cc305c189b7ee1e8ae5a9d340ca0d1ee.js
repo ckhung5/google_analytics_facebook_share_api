882661dd38df82f3f312887ef48a8ef8962b2ddf
Cheah Kok Hung <ckhung5@gmail.com>
2017-04-26 23:04:28 +1000

    added jquery min

--- public/assets/application-6e5849b4eae0aa95eb17b37d1d61a162cc305c189b7ee1e8ae5a9d340ca0d1ee.js
+++ public/assets/application-6e5849b4eae0aa95eb17b37d1d61a162cc305c189b7ee1e8ae5a9d340ca0d1ee.js
@@ -14391,11 +14391,12 @@ $(window).scroll(function() {
 //jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $(document).on('click', 'a.page-scroll', function(event) {
+        event.preventDefault();
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
-        event.preventDefault();
+        
     });
 });
 (function() {
