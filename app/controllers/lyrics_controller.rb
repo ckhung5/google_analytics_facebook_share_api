49c54b135696decf34491cf12f5661da3517772d
Cheah Kok Hung <ckhung5@gmail.com>
2017-04-29 17:20:09 +1000

    separate application html to index and lyrics

--- app/controllers/lyrics_controller.rb
+++ app/controllers/lyrics_controller.rb
@@ -2,7 +2,10 @@ class LyricsController < ApplicationController
   def show
   	if params[:lyrics].present?
   	@lyrics = "lyrics/" + params[:lyrics]
-  	
+  	@metatitle = params[:lyrics].capitalize
+  	@metadescription = @metatitle + " is blah blah "
+
+  	@metaurl = "https://upperoom-united.herokuapp.com/show.html?lyrics="+ params[:lyrics]
   	end
   	
   end
