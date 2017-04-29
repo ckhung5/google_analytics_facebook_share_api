9873cacd2337e88b793e06de043cab5c076e6163
Cheah Kok Hung <ckhung5@gmail.com>
2017-04-29 17:29:54 +1000

    edited lyrics controller for description

--- app/controllers/lyrics_controller.rb
+++ app/controllers/lyrics_controller.rb
@@ -3,9 +3,28 @@ class LyricsController < ApplicationController
   	if params[:lyrics].present?
   	@lyrics = "lyrics/" + params[:lyrics]
   	@metatitle = params[:lyrics].capitalize
-  	@metadescription = @metatitle + " is blah blah "
-
   	@metaurl = "https://upperoom-united.herokuapp.com/show.html?lyrics="+ params[:lyrics]
+
+
+  	if params[:lyrics] == "lyrics1"
+  		@metadescription = @metatitle + " lyrics1 description "
+  	elsif params[:lyrics] == "lyrics2"
+  		@metadescription = @metatitle + " lyrics2 description "
+  	elsif params[:lyrics] == "lyrics3"
+  		@metadescription = @metatitle + " lyrics3 description "
+  	elsif params[:lyrics] == "lyrics4"
+  		@metadescription = @metatitle + " lyrics4 description "
+  	elsif params[:lyrics] == "lyrics5"
+  		@metadescription = @metatitle + " lyrics5 description "
+  	elsif params[:lyrics] == "lyrics6"
+  		@metadescription = @metatitle + " lyrics6 description "	
+  	else
+  		@metadescription = @metatitle + " :no description yet "
+  	end
+  		
+  		
+
+  	
   	end
   	
   end
