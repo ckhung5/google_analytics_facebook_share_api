61e03673b0b925675ee9b043350697f3e2f54b7d
Cheah Kok Hung <ckhung5@gmail.com>
2017-04-29 11:04:27 +1000

    added fb init

--- app/assets/javascripts/scrolling-nav.js
+++ app/assets/javascripts/scrolling-nav.js
@@ -19,3 +19,22 @@ $(function() {
 });
 
 
+
+
+
+document.getElementById('song1-shareBtn').onclick = function() {
+  FB.ui({
+    method: 'share',
+    mobile_iframe: true,
+    href: 'https://upperoom-united.herokuapp.com/lyrics/show.html?lyrics=lyrics1',
+  },  // callback
+  function(response) {
+    if (response && !response.error_message) {
+      alert('Posting completed.');
+    } else {
+      alert('Error while posting.');
+    }
+  }
+);
+}
+
